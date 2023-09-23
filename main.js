c827ba5201013acdd6dc3d7120a90aaf5ee1910e
Eduardo Mazelli <eduardo.mazelli@gmail.com>
2023-09-23 20:12:57 -0300

    atualização files

---
+++ main.js
@@ -0,0 +1,43 @@
+// Imprime uma mensagem na tela
+print("Hello World!")
+
+// Imprime uma mensagem no console
+console.log("Hello World!")
+
+// 'Typeof' nos devolvera qual é o tipo da variavel que estamos lhe entregando 
+console.log(typeof "Hello World!")
+
+// Listas são estrutura de dados que podemos recuperar atraves de indexes. Ajudando a deixar o código mais organizado e limpo
+list = [1, 2, 3, 4]
+
+// Laços de repetição
+for(let i = 0, i = 10, i = i + 1){
+    // código
+}
+
+// Cria o 'canvas' na página da web para podermos criar os designs
+createCanvas(x,y) // x (horizontal) e y (vertical) significaram o tamanho do canvas
+
+// Define a cor do fundo do canvas
+background(z) // Z mais perto de 0 mais escuro, mais longe de 0 mais claro. z = 0 = preto
+
+// Cria variavel
+let variavel // Depois do 'let' devemos começar o nome da váriavel com letra minúscula
+
+// Função 'if' que usamos para criar um condição se para ser checada no código
+if(condicao1 || condicao2 && condicao3){ // As '||' significa 'ou' | As '&&' significa 'e'
+    // ação a ser realizada
+}else{
+    // ação a ser realizada
+} //Usado após uma condição 'if' que seta que ação deve ocorrer caso a condição se anterior não seja atingida
+
+// Usamos '//' para comentar uma linha no código. Isso fara com que o leitor ignore a linha
+
+// Muda a cor
+variavel.fill
+
+// Tamanho do Texto
+variavel.textsize
+
+// Alinha o texto
+variavel.textaling
